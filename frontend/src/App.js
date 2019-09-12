dacf75f55e92fe719cf5fc6d10ff92cfd2ad9c43
William Thalisson <william.t1997@gmail.com>
2019-09-12 15:07:30 -0300

    Initial configuration for the landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
-import './App.css';
+import LandingPage from "./components/LandingPage";
+import header from "./components/header";
+import footer from "./components/footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/welcome" component={header} />
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                Xablau jhgjhgjhg
+            </div>
+        )
+    }
+}
+
+export default LandingPage
