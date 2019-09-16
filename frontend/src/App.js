08aefa26f85233d18793763925f8dd16268a869e
puerto229 <vinicius.alves.porto@gmail.com>
2019-09-16 20:16:44 -0300

    Intalled the react-router and put a model in the App.js and created the folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* Import and route your components just like the model */}
+        {/* <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />  */}
     </div>
   );
 }
