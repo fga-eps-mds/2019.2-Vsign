19272034c280029d54e801accb92cb499dd8a615
puerto229 <vinicius.alves.porto@gmail.com>
2019-09-18 15:25:18 -0300

    Put styled-components in separeted file

--- frontend/src/components/recordUserVideo/recordUserVideo-view.js
+++ frontend/src/components/recordUserVideo/recordUserVideo-view.js
@@ -4,7 +4,7 @@ import { withRouter, Redirect } from 'react-router-dom';
 // import { bindActionCreators } from 'redux';
 // import { connect } from 'react-redux';
 import styled from 'styled-components'
-
+import {Container, RowOnTop, IntructionTextBtn, InstructionBtn} from "./styled_components.js"
 // import { Container } from './styles';
 
 class recordUserVideo extends Component {
@@ -21,32 +21,7 @@ class recordUserVideo extends Component {
 
     
   render() {
-    const Container = styled.div`
-            display: flex;
-            flex-direction: column;
-            `;
-
-    const InstructionBtn = styled.div`
-                display: flex;
-                background-color: white;
-                height: 55px;
-                width: 337px;
-                border-style: solid;
-                border-color: black;
-                border-width: 1px;
-                border-radius: 28px;
-                align-items: center;
-                justify-content: center
-            `;
-    const IntructionTextBtn = styled.p`
-            display: flex;
-            font-size: 30;
-            font-weight: bold;
-            font-family: Arial, Helvetica, sans-serif;
-            color: black;
-            align-self: center
-
-            `;
+    
     const RecordingAlert = styled.div`
             display: flex;
             background-color: red;
@@ -60,13 +35,7 @@ class recordUserVideo extends Component {
             justify-content: center
         `;
 
-    const RowOnTop = styled.div`
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-        align-items: center;
-        
-        `;
+    
 
        
     return (
---
+++ frontend/src/components/recordUserVideo/styled_components.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const InstructionBtn = styled.div`
+    display: flex;
+    background-color: white;
+    height: 55px;
+    width: 337px;
+    border-style: solid;
+    border-color: black;
+    border-width: 1px;
+    border-radius: 28px;
+    align-items: center;
+    justify-content: center
+`;
+export const IntructionTextBtn = styled.p`
+    display: flex;
+    font-size: 30;
+    font-weight: bold;
+    font-family: Arial, Helvetica, sans-serif;
+    color: black;
+    align-self: center
+    `;
+
+export const RowOnTop = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+
+    `;
