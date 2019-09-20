991fe7899fb6586e794077a575d222e96e5b951b
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-09-20 01:53:29 -0300

    Added mobile first styles

--- frontend/src/components/UserContracts/Contract.js
+++ frontend/src/components/UserContracts/Contract.js
@@ -7,25 +7,39 @@
 import React from 'react';
 import "./ContractsPage.css";
 
+const checkScreenWidth = () => {
+    const width = window.screen.width;
+    
+    console.log("Width", width);
+    return width;
+}
+
+const goToDetailsScreen = () => {
+    alert("Go to contract details screen");
+}
+
 const contract = props => (
-  <div className="contract">
-    <div className="contract-info">
-      <div className="contract-info-left-div">
-        <p><strong>Número</strong>: {props.contract.uuid}</p>
-        <p><strong>Valor</strong>: R${props.contract.amount}</p>
-      </div>
+    <div className="contract">
+        <div className="contract-info" onClick={() => checkScreenWidth() < 600 ? goToDetailsScreen() : null}>
+            <div className="contract-info-left-div">
+                <p><strong>Número</strong>: {props.contract.uuid}</p>
 
-      <div className="contract-info-right-div">
-        <p>
-          <strong>Status</strong>: {props.contract.status}
-        </p>
-      </div>
+                <p><strong>Valor</strong>: R${props.contract.amount}</p>
+            </div>
 
-      <button>
-        <p>Ver Mais</p>
-      </button>
+            <div className="contract-info-right-div">
+                <p>
+                    <strong>Status</strong>: {props.contract.status}
+                </p>
+            </div>
+
+            {checkScreenWidth() > 600 ? (
+                <button onClick={() => goToDetailsScreen()}>
+                    <p>Ver Mais</p>
+                </button>
+            ) : null}
+        </div>
     </div>
-  </div>
 );
 
 export default contract;
