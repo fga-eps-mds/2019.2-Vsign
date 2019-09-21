936f4ac4bac8751219bbb3139012cb3d3695df7e
Thiago Pereira <thiagoflames@gmail.com>
2019-09-21 10:49:21 -0300

    Insert Style Guide Airbnb lint with Eslint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
+import './App.css'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        {/* Import and route your components just like the model */}
-        {/* <Route exact path="/" component={Home} />
+      {/* Import and route your components just like the model */}
+      {/* <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/topics" component={Topics} />  */}
     </div>
