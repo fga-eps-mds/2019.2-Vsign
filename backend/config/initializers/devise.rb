e85737e0a5494f7e92f7d7e8563d59626aa073f0
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-09-23 17:55:21 -0300

    Configured devise.rb to dispatch JWT tokens and configured routes for user registration and login
    Co-authored-by: @Foxtrot40

--- backend/config/initializers/devise.rb
+++ backend/config/initializers/devise.rb
@@ -3,6 +3,19 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.credentials.secret_key_base
+      
+    jwt.dispatch_requests = [
+      ['POST', %r{ˆ/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 1.day.to_i
+  end
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,4 +6,15 @@ Rails.application.routes.draw do
     resources :contracts, only: [:create]
   end
 
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registration: 'signup'
+             },
+             controllers: {
+               sessions: 'sessions',
+               registrations: 'registrations'
+             }
 end
