3203d867d7c27a98aea7c7eb97f485ec9e39d60e
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-09-23 17:51:33 -0300

    Added devise methods to user
    Co-authored-by: @Foxtrot40

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
   extend Devise::Models
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: JWTBlacklist
         
 end
   
