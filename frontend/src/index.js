5fed2fad8cd73e848ba9d7e66715bcaab8a1fd84
marcosgtavares <marcosgabriel.ro@hotmail.com>
2019-09-24 12:09:39 -0300

    replacing react-apollo for @apollo/react-hooks Co-authored-by: @kairon-v

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
+
+
+const asd = new ApolloClient({
+    uri: 'https://48p1r2roz4.sse.codesandbox.io' 
+});
+
+asd.query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+}).then(result => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
