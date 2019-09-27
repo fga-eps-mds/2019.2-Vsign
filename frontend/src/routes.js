321fe239d8c160fef79b67d51481d5e1b0b9f7ea
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-09-27 19:27:58 -0300

    adding signing tips to routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 import LandingPage from './components/LandingPage';
 import UserContracts from './components/UserContracts/ContractsPage';
+import SigningTips from './components/SigningTips/SigningTips';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" exact component={LandingPage} />
       <Route path="/contracts" component={UserContracts} />
+      <Route path='/tips' component={SigningTips} />
     </BrowserRouter>
   );
 }
