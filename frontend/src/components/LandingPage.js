91938f57630a202ed8abef0a1c6887bdff88ca40
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-09-28 00:45:04 -0300

    first commit

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 
+import { Button } from 'rsuite';
+
 export class LandingPage extends Component {
     render() {
         return (
-            <div>
-                Xablau jhgjhgjhg
-            </div>
+            <Button>Hello World</Button>
         )
     }
 }
