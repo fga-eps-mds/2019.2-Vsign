1d4f92cd66be6e2087a77f2f1dfc8da70c11e451
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-09-28 02:08:11 -0300

    configuring rsuite and adding basic navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Routes from './routes';
 
+import 'rsuite/dist/styles/rsuite-default.css'; // or 'rsuite/dist/styles/rsuite-default.css'
+
 const App = () => <Routes />
 
 export default App;
--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
+import { Button, Container, Navbar, Nav, Dropdown, Icon, Content, Footer, Grid, Row, Col, Panel, PanelGroup } from 'rsuite';
 
-import { Button } from 'rsuite';
 
 export class LandingPage extends Component {
     render() {
         return (
-            <Button>Hello World</Button>
+            <Container>
+                <Navbar>
+                    <Navbar.Header>
+                        <a href="#" className="navbar-brand logo">VSign</a>
+                    </Navbar.Header>
+                    <Navbar.Body>
+                        <Nav>
+                            {/* <Nav.Item icon={<Icon icon="home" />} >Home</Nav.Item>
+                            <Nav.Item>News</Nav.Item>
+                            <Nav.Item>Products</Nav.Item> */}
+                            {/* <Dropdown title="About">
+                                <Dropdown.Item>Company</Dropdown.Item>
+                                <Dropdown.Item>Team</Dropdown.Item>
+                                <Dropdown.Item>Contact</Dropdown.Item>
+                            </Dropdown> */}
+                        </Nav>
+                        <Nav pullRight>
+                            <Nav.Item icon={<Icon icon='cog' />}>Login</Nav.Item>
+                            <Nav.Item icon={<Icon icon="cog" />} >Settings</Nav.Item>
+                        </Nav>
+                    </Navbar.Body>
+                </Navbar>
+                <Content>
+                    <Grid fluid>
+                        <Row className="show-grid">
+                            <Panel bordered>
+                                {/* <Paragraph /> */}
+                            </Panel>
+                        </Row>
+                    </Grid>
+                </Content>
+            </Container>
         )
     }
 }
