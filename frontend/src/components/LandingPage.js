a19c9b39dbd11ba5973de044d273601e825b569c
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-09-28 06:34:36 -0300

    Finished the navbar

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,31 +1,45 @@
-import React, { Component } from 'react'
-import { Button, Container, Navbar, Nav, Dropdown, Icon, Content, Footer, Grid, Row, Col, Panel, PanelGroup } from 'rsuite';
+import React, { useState } from 'react'
+import { Container, Content, Grid, Row, Panel } from 'rsuite';
 import './LandingPage.css';
-export class LandingPage extends Component {
-    render() {
-        return (
-            <Container>
-                <div className="header-bar">
-                    <div className='header-bar-logo'>
-                        <a href="/">VSign</a>
-                    </div>
-                    <div className='header-info'>
-                        <a href="#about">About |</a>
-                        <a href="/login"> Login</a>
-                    </div>
+import Login from './LoginModal/Login';
+
+const LandingPage = () => {
+    const [loginModal, setLoginModal] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <Container>
+            <div className="header-bar">
+                <div className='header-bar-logo'>
+                    <a href="/">VSign</a>
                 </div>
-                <Content>
-                    <Grid fluid>
-                        <Row className="show-grid">
-                            <Panel bordered>
-                                {/* <Paragraph /> */}
-                            </Panel>
-                        </Row>
-                    </Grid>
-                </Content>
-            </Container>
-        )
-    }
+                <div className='header-info'>
+                    <a href="#about">About |</a>
+                    <button onClick={() => setLoginModal(!loginModal)}> Login</button>
+                </div>
+            </div>
+            <Content>
+                <Grid fluid>
+                    <Row className="show-grid">
+                        <Panel bordered>
+                            {/* <Paragraph /> */}
+                        </Panel>
+                    </Row>
+                </Grid>
+            </Content>
+
+            {loginModal ? (
+                <Login 
+                    setLoginModal={setLoginModal} 
+                    email={email} 
+                    password={password} 
+                    setEmail={setEmail} 
+                    setPassword={setPassword} 
+                />
+             ) : null}
+        </Container>
+    )
 }
 
-export default LandingPage
+export default LandingPage;
