be20488fcc07b2d446429db7888d48f2d31b15a5
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-09-28 04:45:04 -0300

    created basic navbar template

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,32 +1,19 @@
 import React, { Component } from 'react'
 import { Button, Container, Navbar, Nav, Dropdown, Icon, Content, Footer, Grid, Row, Col, Panel, PanelGroup } from 'rsuite';
-
-
+import './LandingPage.css';
 export class LandingPage extends Component {
     render() {
         return (
             <Container>
-                <Navbar>
-                    <Navbar.Header>
-                        <a href="#" className="navbar-brand logo">VSign</a>
-                    </Navbar.Header>
-                    <Navbar.Body>
-                        <Nav>
-                            {/* <Nav.Item icon={<Icon icon="home" />} >Home</Nav.Item>
-                            <Nav.Item>News</Nav.Item>
-                            <Nav.Item>Products</Nav.Item> */}
-                            {/* <Dropdown title="About">
-                                <Dropdown.Item>Company</Dropdown.Item>
-                                <Dropdown.Item>Team</Dropdown.Item>
-                                <Dropdown.Item>Contact</Dropdown.Item>
-                            </Dropdown> */}
-                        </Nav>
-                        <Nav pullRight>
-                            <Nav.Item icon={<Icon icon='cog' />}>Login</Nav.Item>
-                            <Nav.Item icon={<Icon icon="cog" />} >Settings</Nav.Item>
-                        </Nav>
-                    </Navbar.Body>
-                </Navbar>
+                <div className="header-bar">
+                    <div className='header-bar-logo'>
+                        <a href="/">VSign</a>
+                    </div>
+                    <div className='header-info'>
+                        <a href="#about">About |</a>
+                        <a href="/login"> Login</a>
+                    </div>
+                </div>
                 <Content>
                     <Grid fluid>
                         <Row className="show-grid">
