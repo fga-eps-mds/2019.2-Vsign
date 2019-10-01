7cc63e25c1681a1ec7e73c2cad796439c55979c2
Kairon Velozo <kairon333@gmail.com>
2019-10-01 01:11:01 -0300

    Adding how it works section of landing page.

---
+++ frontend/src/components/LandingPage/HowItWorks.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { FlexboxGrid, Timeline, Icon, Col } from 'rsuite';
+import { SectionHeader, Section, SectionTitle, SectionSubTitle, StepTitle, StepText, StepItem } from './styles';
+import HowItWorksImage from '../../assets/images/how-it-works.png';
+
+export default function HowItWorks() {
+    return (
+        <Section>
+            <FlexboxGrid justify="space-around">
+                <FlexboxGrid.Item colspan={15}>
+					<SectionHeader>
+                        <SectionTitle>Como Funciona</SectionTitle>
+                        <SectionSubTitle>Assinatura automatizada e segura para seus contratos.</SectionSubTitle>
+                    </SectionHeader>
+                	<FlexboxGrid justify="space-between" align="middle">
+						<FlexboxGrid.Item componentClass={Col} colspan={24} md={12} className="text-center">
+							<img src={HowItWorksImage} width="90%" />
+						</FlexboxGrid.Item>
+                    	<FlexboxGrid.Item componentClass={Col} colspan={24} md={10} mdPull={2}>
+                    		<Timeline className="how-it-works">
+    							<Timeline.Item dot={<Icon icon="check-circle" style={{ color: 'green' }} />}>
+      								<StepTitle>Integração</StepTitle>
+      								<StepText>Integre seu sistema com a Vsign através de nossa API.</StepText>
+    							</Timeline.Item>
+								<Timeline.Item dot={<Icon icon="exclamation-triangle" style={{ color: 'orange' }} />}>
+									<StepTitle>Envio</StepTitle>
+									<StepText>Envie seus contratos para nossa plataforma.</StepText>
+								</Timeline.Item>
+								<Timeline.Item dot={<Icon icon="info-circle" style={{ color: 'blue' }} />}>
+									<StepTitle>Assinatura</StepTitle>
+									<StepText>Seus clientes realizam a assinatura através da gravação de um vídeo em nossa plataforma.</StepText>
+								</Timeline.Item>
+								<Timeline.Item dot={<Icon icon="check-circle" style={{ color: 'green' }} />}>
+									<StepTitle>Resultado</StepTitle>
+									<StepText>Notificamos você logo após a assinatura do contrato.</StepText>
+								</Timeline.Item>
+  							</Timeline>
+                    </FlexboxGrid.Item>
+
+                </FlexboxGrid>
+            </FlexboxGrid.Item>
+                </FlexboxGrid>
+        </Section>
+    );
+};
