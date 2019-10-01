f95e5bedb03545c9b105f3733438326dd8105c4b
Thiago Pereira <thiagoflames@gmail.com>
2019-10-01 18:03:28 -0300

    Insert test example and prepare the test environmet

--- backend/spec/models/company_spec.rb
+++ backend/spec/models/company_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context "attributes" do
+  
+    it "has a name" do 
+    company = Company.create!(name: "My Awesome Company Name") # creating a new company 'instance'
+    expect(company.name).to eq("My Awesome Company Name") # this is our expectation
+    end
+    
+  end
 end
