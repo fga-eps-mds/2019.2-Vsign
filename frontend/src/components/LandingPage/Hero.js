82cd6d8c35d2193abda41f05d5fa7f146500fa94
Kairon Velozo <kairon333@gmail.com>
2019-10-01 02:30:38 -0300

    Adding hero section to the landing page.

--- frontend/src/components/LandingPage/Hero.js
+++ frontend/src/components/LandingPage/Hero.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import { FlexboxGrid,  } from 'rsuite';
+import { FlexboxGrid, Button, Col } from 'rsuite';
+import { StyledHero } from './styles';
+import { Title, SubTitle } from './styles';
+import HeroImage from '../../assets/images/hero.png';
 
 export default function Hero() {
     return (
-        <FlexboxGrid justify="center">
-          <FlexboxGrid.Item colspan={12}>
-            <Panel header={<h3>Login</h3>} bordered>
-              <Form fluid>
-                <FormGroup>
-                  <ControlLabel>Username or email address</ControlLabel>
-                  <FormControl name="name" />
-                </FormGroup>
-                <FormGroup>
-                  <ControlLabel>Password</ControlLabel>
-                  <FormControl name="password" type="password" />
-                </FormGroup>
-                <FormGroup>
-                  <ButtonToolbar>
-                    <Button appearance="primary">Sign in</Button>
-                    <Button appearance="link">Forgot password?</Button>
-                  </ButtonToolbar>
-                </FormGroup>
-              </Form>
-            </Panel>
-          </FlexboxGrid.Item>
-        </FlexboxGrid>
+        <StyledHero>
+            <FlexboxGrid justify="center">
+                <FlexboxGrid.Item colspan={15}>
+                    <FlexboxGrid justify="space-between" align="middle">
+                        <FlexboxGrid.Item componentClass={Col} colspan={24} md={10}>
+                            <Title>Assine contratos com segurança</Title>
+                            <SubTitle>Assinatura através de gravação de vídeo com análise automática e notificação dos resultados em tempo real.</SubTitle>
+                            <Button size="lg">Contrate Agora</Button>
+                        </FlexboxGrid.Item>
+                        <FlexboxGrid.Item componentClass={Col} colspan={24} md={13} mdPush={1}>
+                            <img src={HeroImage} width="100%" />
+                        </FlexboxGrid.Item>
+                    </FlexboxGrid>
+                </FlexboxGrid.Item>
+            </FlexboxGrid>
+        </StyledHero>
     );
 };
