59f7305c62f93a8fc0a2d00108e3e9478f89639a
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-10-02 17:58:31 -0300

    migrated

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_210527) do
+ActiveRecord::Schema.define(version: 2019_10_02_204644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -29,7 +50,9 @@ ActiveRecord::Schema.define(version: 2019_09_29_210527) do
     t.string "order"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
     t.index ["company_id"], name: "index_contracts_on_company_id"
+    t.index ["token"], name: "index_contracts_on_token", unique: true
     t.index ["user_id"], name: "index_contracts_on_user_id"
   end
 
@@ -61,6 +84,7 @@ ActiveRecord::Schema.define(version: 2019_09_29_210527) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "contracts", "companies"
   add_foreign_key "contracts", "users"
   add_foreign_key "scripts", "companies"
