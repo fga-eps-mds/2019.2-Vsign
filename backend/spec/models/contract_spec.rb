05dcdabc58e292e2611821234fb1272740525b31
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-10-02 18:12:07 -0300

    Trying to test, missing rails_helper file

--- backend/spec/models/contract_spec.rb
+++ backend/spec/models/contract_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Contract, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    expect(Contract.new).to be_valid
+  end
 end
