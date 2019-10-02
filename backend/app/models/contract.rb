81932931741aa67487547f152145544ad9de6cb6
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-10-02 18:11:47 -0300

    Contracts now creates an encrypted token before saving

--- backend/app/models/contract.rb
+++ backend/app/models/contract.rb
@@ -1,6 +1,14 @@
+require 'bcrypt'
+
 class Contract < ApplicationRecord
+    before_create :set_token
     
     belongs_to :company
     belongs_to :user
     
+
+    def set_token
+        self.token = BCrypt::Password.new(SecureRandom.urlsafe_base64(12))
+        break unless Contract.where(token: token).exists?
+    end
 end
