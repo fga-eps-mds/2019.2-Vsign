152d4f779b0f81f51256473f46ca16189ef9902e
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-10-06 19:45:41 -0300

    Removing validation of token as the application creates it. Changing how the token is created so it's saved URL friendly

--- backend/app/models/contract.rb
+++ backend/app/models/contract.rb
@@ -1,16 +1,30 @@
-require 'bcrypt'
-
 class Contract < ApplicationRecord
+    include BCrypt
+    
     before_create :set_token
     
     belongs_to :company
     belongs_to :user
 
-    validates_presence_of :order, :script, :token
+    validates_presence_of :order, :script
+
+    # Should be uncommented if crew decides to use rails native
+    # frontend for creating Contracts
+    # def to_param
+    #     token
+    # end
     
     def set_token
         loop do
-            self.token = BCrypt::Password.new(SecureRandom.urlsafe_base64(12))
+            token = Password.create(SecureRandom.urlsafe_base64(12))
+            
+            # Removing special characteres that will
+            # break url
+            ["$", "/", "."].each do |str|
+                token = token.delete str
+            end
+
+            self.token = token
             break unless Contract.where(token: token).exists?
         end
     end
