dcd42bced420c0b6eaf26e0cc9eb654400f85b6f
William Thalisson <william.t1997@gmail.com>
2019-10-07 23:50:35 -0300

    Solving issues with merge on gemfile.lock

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import Routes from './routes';
 import GlobalStyle from './styles';
 
@@ -10,21 +9,4 @@ const App = () => (
     </React.Fragment>
 );
 
-=======
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Script from './components/Script';
-
-function App() {
-  return (
-    <Router>
-        {/* Import and route your components just like the model */}
-        {/* <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />  */}
-        <Route exact path="/scriptpage" component={Script}/>
-    </Router>
-  );
-}
->>>>>>> 2944ef85f093c8ebb0421c3af509568f7ba00439
 export default App;
