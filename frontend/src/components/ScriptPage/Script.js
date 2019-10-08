3ef06cf68721a8eecc1ba4a541a2fe6d7d342715
William Thalisson <william.t1997@gmail.com>
2019-10-07 23:11:06 -0300

    Report page text configuration

---
+++ frontend/src/components/ScriptPage/Script.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react'
+import './ScriptPage.css';
+import camera_icon from '../../assets/images/camera-icon.png';
+import user_icon from '../../assets/images/user-icon.png';
+import {
+    BrowserRouter as Router,
+    withRouter,
+    Switch,
+    Route,
+  } from "react-router-dom";
+
+
+
+function recordVideo(props){
+    props.history.push('/')
+}
+
+function relataErro(props){
+    props.history.push({
+      pathname: `/scriptpage/reportar`,
+      state: { modal: true }
+    })
+  }
+
+export class Script extends Component {
+    previousLocation = this.props.location;
+
+  componentWillUpdate(nextProps) {
+    let { location } = this.props;
+
+    if (
+      nextProps.history.action !== "POP" &&
+      (!location.state || !location.state.modal)
+    ) {
+      this.previousLocation = this.props.location;
+    }
+  }
+    render() {
+        let { location } = this.props;
+
+        let isModal = !!(
+        location.state &&
+        location.state.modal &&
+        this.previousLocation !== location
+        );
+        return (
+            <div className='container borda'>
+                <div className='cabecalho borda'>
+                    <img src={user_icon} alt=''/>
+                    <p>Contrato Nº###</p>
+
+                </div>
+                <div className='instrucao borda'>
+                    <p>Para assinar o contrato você deve ler o roteiro
+                        abaixo e gravar um video aqui mesmo na nossa plataforma.
+                    </p>
+                </div>
+                <div className="tela borda">
+                    <h2>Roteiro</h2>
+                    <p>(exibir o roteiro aqui)</p>
+                </div>
+
+                <button className="gravar borda" onClick={() => recordVideo(this.props)}>
+                    <p>Próximo</p>
+                    <img src={camera_icon} alt="" />
+                </button>
+                <div>
+                    <Switch location={isModal ? this.previousLocation : location}>
+                        <div>
+                            <button className='erro borda' onClick={() => relataErro(this.props)}>
+                                <p>Reportar erro no texto do roteiro</p>
+                            </button>
+                        </div>
+                    </Switch>
+                    {isModal ? <Route path="/scriptpage" component={Modal} /> : null}
+                </div>
+            </div>
+        )
+    }
+}
+function Modal({history }) {
+
+    let back = e => {
+      e.stopPropagation();
+      history.goBack();
+    };
+  
+    return (
+      <div className='modalbk'>
+        <div className="modal">
+          <h1>Relate o erro encontrado</h1>
+          <div>
+            <input id='mytext' className='relatar' type='text'/>
+          </div>
+          <div className='Buttons'>
+              <button className='cancelButton' onClick={back}>
+                <p>Cancelar</p>
+              </button>
+              <button className='sendButton' onClick={back}>
+                <p>Enviar</p>
+              </button>
+          </div>
+            
+        </div>
+      </div>
+    );
+  }
+  
+  function ScriptPage() {
+    return (
+      <Router>
+        <Route component={Script} />
+      </Router>
+    );
+  }
+  
+export default ScriptPage;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import LandingPage from './components/LandingPage';
 import LoginPage from './components/LoginPage';
 import ContractsPage from './components/ContractsPage';
 import RecordVideoPage from './components/recordUserVideo';
+import ScriptPage from './components/ScriptPage/Script.js';
 
 export default function Routes() {
   return (
@@ -12,6 +13,7 @@ export default function Routes() {
       <Route path="/login" exact component={LoginPage} />
       <Route path="/contracts" component={ContractsPage} />
       <Route path="/record" component={RecordVideoPage} />
+      <Route path="/script" component={ScriptPage} />
     </BrowserRouter>
   );
 }
