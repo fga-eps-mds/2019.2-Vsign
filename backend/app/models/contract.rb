f2427676c422fc675688c28513c5bf50d545b215
puerto229 <vinicius.alves.porto@gmail.com>
2019-10-26 13:07:53 -0300

    Uploads

--- backend/app/models/contract.rb
+++ backend/app/models/contract.rb
@@ -3,4 +3,8 @@
 class Contract < ApplicationRecord
   belongs_to :company
   belongs_to :user
+
+  has_one_attached :contract_video
+  has_one_attached :contract_audio
+  has_many_attached :contract_image
 end
