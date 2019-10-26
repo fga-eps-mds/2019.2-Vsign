a71c28b34031e003fc47d0243cb35f7c2046e951
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-10-26 14:16:25 -0300

    login on backend

--- backend/config/initializers/devise.rb
+++ backend/config/initializers/devise.rb
@@ -5,14 +5,15 @@
 Devise.setup do |config|
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.secret_key_base
+    # jwt.secret = Rails.application.credentials.secret_key_base
+    jwt.secret = "teste123"
       
-    # jwt.dispatch_requests = [
-    #   ['POST', %r{ˆ/login$}]
-    # ]
-    # jwt.revocation_requests = [
-    #   ['DELETE', %r{^/logout$}]
-    # ]
+    jwt.dispatch_requests = [
+      ['POST', %r{ˆ/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
     jwt.expiration_time = 1.day.to_i
   end
 
