451da2e9d91d311642098fd064788d527af4d1b6
puerto229 <vinicius.alves.porto@gmail.com>
2019-11-02 12:28:56 -0300

    Extracting from attached file

--- backend/app/jobs/extract_audio_text_job.rb
+++ backend/app/jobs/extract_audio_text_job.rb
@@ -1,7 +1,18 @@
 class ExtractAudioTextJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(*contract)
+    if contract.audio.attached?
+      require "google/cloud/speech"
+
+      binary = user.avatar.download
+      speech = Google::Cloud::Speech.new
+      config = { encoding:          :FLAC,
+          audio_channel_count: 2,
+           language_code:     "pt-BR" }
+      audio  = { content: binary }
+      response = speech.recognize(config, audio)
+      text = response.results.alternatives.to_s    
+    end
   end
 end
