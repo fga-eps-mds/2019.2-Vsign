6d36e95f0a028e592eac77ff669f7c519695d49d
Kairon Velozo <kairon333@gmail.com>
2019-11-02 14:49:47 -0300

    Creating set record assets actions.

---
+++ frontend/src/actions/record.js
@@ -0,0 +1,20 @@
+export const setRecordVideoAction = (video) => ({
+    type: 'SET_VIDEO',
+    payload: {
+        video
+    }
+});
+
+export const setRecordAudioAction = (video) => ({
+    type: 'SET_VIDEO',
+    payload: {
+        video
+    }
+});
+
+export const setRecordImagesAction = (video) => ({
+    type: 'SET_IMAGES',
+    payload: {
+        video
+    }
+});
