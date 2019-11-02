8af250e13ca92216d476890429a5d11493c44727
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-02 13:53:08 -0300

    Configured Amazon S3

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -29,8 +27,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  # Store files on Amazon S3.
+  config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
