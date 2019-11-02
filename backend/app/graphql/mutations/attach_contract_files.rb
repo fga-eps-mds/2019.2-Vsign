19f74311413a3063d10c5899955042aaf548bc24
puerto229 <vinicius.alves.porto@gmail.com>
2019-11-02 11:43:01 -0300

    Enqueue the the job on mutation

--- backend/app/graphql/mutations/attach_contract_files.rb
+++ backend/app/graphql/mutations/attach_contract_files.rb
@@ -17,6 +17,7 @@ module Mutations
       contract.audio.attach(files[audio])
 
       images.each {|image| contract.image.attach(image)}
+      ExtractAudioTextJob.perform_later contract
     end
   end
 end
