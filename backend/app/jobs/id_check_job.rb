c2bed5b3760efcae29021f03989cdc5d2e6e00da
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-03 13:39:28 -0300

    Used AnalyzeDocument

--- backend/app/jobs/id_check_job.rb
+++ backend/app/jobs/id_check_job.rb
@@ -2,26 +2,28 @@ class IdCheckJob < ApplicationJob
   queue_as :default
   
   def perform(*args)
-    client = Aws::Rekognition::Client.new({
+    client = Aws::Textract::Client.new({
       region: Rails.application.credentials.dig(:aws, :region),
       credentials: Aws::Credentials.new(
         Rails.application.credentials.dig(:aws, :access_key_id),
         Rails.application.credentials.dig(:aws, :secret_access_key))
     })
-    resp = client.detect_text({
-      image: {
+    resp = client.analyze_document({
+      document: {
         s3_object: {
           bucket: Rails.application.credentials[Rails.env.to_sym][:aws][:bucket],
-          name: 'file' # 'file' =  'IMAGE 2019-10-26 18:26:52.jpg'
+          name: 'IMAGE 2019-10-26 18:26:52.jpg' # 'file' =  'IMAGE 2019-10-26 18:26:52.jpg'
         },
       },
+      feature_types: ["FORMS"]
     })
 
-    resp.text_detections.each do |text|
-      if text.type === "line"
-        puts "#{text.detected_text}-#{text.id}"
-      end
-    end
+    # resp.text_detections.each do |text|
+    #   if text.type === "WORD"
+    #     # puts "#{text.detected_text.to_i}"
+    #     puts "#{text.detected_text}-#{text.id}"
+    #   end
+    # end
 
   end
   
