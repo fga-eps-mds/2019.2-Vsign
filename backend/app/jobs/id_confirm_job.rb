3739872f662b3096642c3397bfa1a0e2a5fa9d6e
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-07 19:50:20 -0300

    Update id_confirm_job.rb

--- backend/app/jobs/id_confirm_job.rb
+++ backend/app/jobs/id_confirm_job.rb
@@ -29,13 +29,51 @@ def perform(contract_id)
       mun_confidence: 90.0
     }
 
-    response = cliente.detect_labels attrs
-    response.labels.each do |labels|
-      position = face_match.face.bounding_box
-      similarity = face_match.similarity
-      puts "#{position.left}, #{position.top} matches with #{similarity}"
+    response_image = cliente.detect_labels attrs
+    response_image.labels.each do |labels|
+      if (labels.name == "Document" || labels.name == "License" || labels.name == "Driving License" || labels.name == "Id Cards")
+        is_document = true
+        what_document?(is_document)
+      else
+        is_document = false
+        @contract.status = "error, not a valid document"
     end
 
+  end
+
+  def what_document? (document_status)
+        
+    client = Aws::Rekognition::Client.new({
+      region: Rails.application.credentials.dig(:aws, :region),
+      credentials: Aws::Credentials.new(
+        Rails.application.credentials.dig(:aws, :access_key_id),
+        Rails.application.credentials.dig(:aws, :secret_access_key))
+    })
+
+    attrs = {
+      image: {
+        s3_object: {
+          bucket: Rails.application.credentials[Rails.env.to_sym][:aws][:bucket],
+          name: document_image
+        },
+      },
+      max_label: 20,
+      mun_confidence: 90.0
+    }
+    
+    response_text = client.detect_text  attrs
+    response_text = text_detection.each do |text|
+      if (text.detect_text == "CPF" && text.detect_text == "NOME" || text.detect_text == "FILIACAO")
+        if (text.detect_text == "PERMISSAO" || text.detect_text == "HAB")
+          id_type = "CNH"
+          
+        else
+          id_type = "RG"
+        end
+      end
+    end
+
+    id_type
   end
 
   def perform_next_job(similarity)
