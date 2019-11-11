513d3d4d95bca2ab3b44bd56b9e7248ddd1a192c
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-11 11:34:07 -0300

    Using amazon rekognition

--- backend/app/jobs/id_check_job.rb
+++ backend/app/jobs/id_check_job.rb
@@ -1,31 +1,33 @@
 class IdCheckJob < ApplicationJob
   queue_as :default
   
-  def perform(*args)
-    client = Aws::Textract::Client.new({
+  def perform(document_id)
+    @contract = Contract.find(contract_id)
+    @user = @contract.user
+    @document = @user.user_document
+    get_text(@document)
+  end
+
+  private 
+
+  def get_text(document_photo)
+    client = Aws::Rekognition::Client.new({
       region: Rails.application.credentials.dig(:aws, :region),
       credentials: Aws::Credentials.new(
         Rails.application.credentials.dig(:aws, :access_key_id),
         Rails.application.credentials.dig(:aws, :secret_access_key))
     })
-    resp = client.analyze_document({
-      document: {
+    
+    attrs = {
+      image: {
         s3_object: {
           bucket: Rails.application.credentials[Rails.env.to_sym][:aws][:bucket],
-          name: 'IMAGE 2019-10-26 18:26:52.jpg' # 'file' =  'IMAGE 2019-10-26 18:26:52.jpg'
+          name: document_photo # 'file' =  'IMAGE 2019-10-26 18:26:52.jpg'
         },
       },
-      feature_types: ["FORMS"]
-    })
-
-    resp.blocks.each do |blocks|
-      text = []
-      if blocks.block_type === "LINE" && blocks.confidence >= 90 #"KEY_VALUE_SET"
-        text = blocks.text
-        puts text
-      end
-    end
-
+    }
+    response = client.detect_text attrs
+    response.text_detections.each do |text|
   end
   
 end
