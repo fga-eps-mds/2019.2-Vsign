5980122310fb78620d313a05f09b5621d997a701
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-11 21:22:53 -0300

    Requested changes

--- backend/app/jobs/face_match_job.rb
+++ backend/app/jobs/face_match_job.rb
@@ -1,24 +1,25 @@
 class FaceMatchJob < ApplicationJob
   queue_as :default
   
-  def perform(contract_id)
-    @contract = Contract.find(contract_id)
-    @user = @contract.user
-    document = @user.user.user_document
-    @contract.image.each |image| face_matching(image.filename, documento.filename)
+  def perform(contract_id) 
     
-  end
-  
-  private 
-  
-  def face_matching(video_image, document_image)
     client = Aws::Rekognition::Client.new({
       region: Rails.application.credentials.dig(:aws, :region),
       credentials: Aws::Credentials.new(
         Rails.application.credentials.dig(:aws, :access_key_id),
         Rails.application.credentials.dig(:aws, :secret_access_key))
     })
-
+    @contract = Contract.find(contract_id)
+    @user = @contract.user
+    document = @user.user.user_document
+    @contract.image.each |image| face_matching(image.filename, document.filename)
+    
+  end
+  
+  private 
+  
+  def face_matching(video_image, document_image)
+   
     attrs = {
       source_image: {
         s3_object: {
@@ -37,11 +38,11 @@ class FaceMatchJob < ApplicationJob
 
     response = cliente.compare_faces attrs
     response.face_matches.each do |face_match|
-      position = face_match.face.bounding_box
       similarity = face_match.similarity
-      puts "#{position.left}, #{position.top} matches with #{similarity}"
     end
   
+    perform_next_job(similarity)
+
   end
 
 
