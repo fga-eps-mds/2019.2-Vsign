eaf1b8d17de79bb3fd767ac2f8f3d398c559dfb9
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-11-12 20:42:04 -0300

    Removed document from contract and added to script as requested

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_02_183305) do
+ActiveRecord::Schema.define(version: 2019_11_12_232725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2019_11_02_183305) do
     t.bigint "company_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "document"
     t.index ["company_id"], name: "index_scripts_on_company_id"
   end
 
