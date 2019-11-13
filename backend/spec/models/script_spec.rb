7d888371740a1da1256e18e421650c2dd1597b9f
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-11-12 21:23:51 -0300

    Creating tests for script_spec.rb

--- backend/spec/models/script_spec.rb
+++ backend/spec/models/script_spec.rb
@@ -4,25 +4,50 @@ require 'rails_helper'
 
 RSpec.describe Script, type: :model do
   context 'validation tests' do
+    subject(:script) { 
+      Script.new(
+        kind: "kind_script",
+        content: '{ "Start":"start", "Num":1, "End":"end" }',
+        title: "Test",
+        document: 0
+      ) 
+    }
+    
     it 'ensures title presence' do
-      script = Script.new(
-        kind: 'kind_script',
-        content: '{ "Start":"start", "Num":1, "End":"end" }'
-      ).save
-      expect(script).to eq(false)
+      script.title = nil
+      script.save
+
+      it { should_not be_valid }
+      # expect(script).to eq(false)
     end
 
     it 'ensures kind presence' do
-      script = Script.new(
-        title: 'title_script',
-        content: '{ "Start":"start", "Num":1, "End":"end" }'
-      ).save
-      expect(script).to eq(false)
+      script.kind = nil
+      script.save
+
+      it { should_not be_valid }
+      # expect(script).to eq(false)
     end
 
     it 'ensures content presence' do
-      script = Script.new(title: 'title_script', kind: 'kind_script').save
-      expect(script).to eq(false)
+      script.content = nil
+      script.save
+
+      it { should_not be_valid }
+      # expect(script).to eq(false)
+    end
+
+    it "ensures document presence" do
+      script.document = nil
+      script.save
+
+      it { should_not be_valid }
+    end
+
+    it "ensures script is valid" do
+      script.save
+
+      it { should be_valid }
     end
   end
 end
