256bd1acbb3a92e46d74ac7a52e8ae31b03d6545
Kairon Velozo <kairon333@gmail.com>
2019-11-13 10:02:50 -0300

    Updating graphql contract queries.

--- backend/app/graphql/types/contract_type.rb
+++ backend/app/graphql/types/contract_type.rb
@@ -4,6 +4,8 @@ module Types
     field :script, String, null: false
     field :order, String, null: false
     field :user, Types::UserType, null: false
+    field :company, Types::CompanyType, null: false
     field :token, String, null: false
+    field :created_at, String, null: false
   end
 end
