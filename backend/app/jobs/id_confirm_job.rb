2243262dd7102c820dbdd41cf533343fffad3653
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-12 22:01:44 -0300

    Update id_confirm_job.rb

--- backend/app/jobs/id_confirm_job.rb
+++ backend/app/jobs/id_confirm_job.rb
@@ -2,22 +2,22 @@ class IdConfirmJob < ApplicationJob
   queue_as :default
 
   def perform(contract_id)
-      @contract = Contract.find(contract_id)
-      @user = @contract.user
-      @document = @user.user_document
-      id_confirm(@document)
-    end
-
-  private 
-
-  def id_confirm(document_image)
-    client = Aws::Rekognition::Client.new({
+    @client = Aws::Rekognition::Client.new({
     region: Rails.application.credentials.dig(:aws, :region),
     credentials: Aws::Credentials.new(
       Rails.application.credentials.dig(:aws, :access_key_id),
       Rails.application.credentials.dig(:aws, :secret_access_key))
     })
 
+    @contract = Contract.find(contract_id)
+    @user = @contract.user
+    @document = @user.user_document
+    id_confirm(@document)
+  end
+
+  private 
+
+  def id_confirm(document_image)
     attrs = {
       image: {
         s3_object: {
@@ -28,8 +28,7 @@ class IdConfirmJob < ApplicationJob
       max_label: 20,
       mun_confidence: 90.0
     }
-
-    response_image = cliente.detect_labels attrs
+    response_image = @cliente.detect_labels attrs
     response_image.labels.each do |labels|
       if (labels.name == "Document" || labels.name == "License" || labels.name == "Driving License" || labels.name == "Id Cards")
         is_document = true
@@ -41,15 +40,7 @@ class IdConfirmJob < ApplicationJob
     end
   end
 
-  def what_document? (document_status)
-        
-    client = Aws::Rekognition::Client.new({
-      region: Rails.application.credentials.dig(:aws, :region),
-      credentials: Aws::Credentials.new(
-        Rails.application.credentials.dig(:aws, :access_key_id),
-        Rails.application.credentials.dig(:aws, :secret_access_key))
-    })
-
+  def what_document? ()    
     attrs = {
       image: {
         s3_object: {
@@ -57,11 +48,8 @@ class IdConfirmJob < ApplicationJob
           name: document_image
         },
       },
-      max_label: 20,
-      mun_confidence: 90.0
     }
-    
-    response_text = client.detect_text  attrs
+    response_text = @client.detect_text  attrs
     response_text = text_detection.each do |text|
       if (text.detect_text == "CPF" && text.detect_text == "NOME" || text.detect_text == "FILIACAO")
         if (text.detect_text == "PERMISSAO" || text.detect_text == "HAB")
