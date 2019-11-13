445962f598af073b447436037a25921c15eedd21
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-11-12 21:53:43 -0300

    Test coverege for validations

--- backend/app/models/script.rb
+++ backend/app/models/script.rb
@@ -4,7 +4,7 @@ class Script < ApplicationRecord
   enum document: { cnh: 0, rg: 1, any: 2 }
   belongs_to :company
 
-  validates_presence_of :document, inclusion: 0..2
+  validates_presence_of :document
   validates_presence_of :kind, :content, :title
 
   def format_content(data)
--- backend/spec/models/script_spec.rb
+++ backend/spec/models/script_spec.rb
@@ -4,12 +4,15 @@ require 'rails_helper'
 
 RSpec.describe Script, type: :model do
   context 'validation tests' do
-    subject(:script) { 
+    subject(:script) {
+      c = Company.create(name: "Teste")
+
       Script.new(
         kind: "kind_script",
         content: '{ "Start":"start", "Num":1, "End":"end" }',
         title: "Test",
-        document: 0
+        document: 0,
+        company_id: c.id
       ) 
     }
     
@@ -17,37 +20,48 @@ RSpec.describe Script, type: :model do
       script.title = nil
       script.save
 
-      it { should_not be_valid }
-      # expect(script).to eq(false)
+      expect(script).to_not be_valid
     end
 
     it 'ensures kind presence' do
       script.kind = nil
       script.save
 
-      it { should_not be_valid }
-      # expect(script).to eq(false)
+      expect(script).to_not be_valid
     end
 
     it 'ensures content presence' do
       script.content = nil
       script.save
 
-      it { should_not be_valid }
-      # expect(script).to eq(false)
+      expect(script).to_not be_valid
     end
 
     it "ensures document presence" do
       script.document = nil
       script.save
 
-      it { should_not be_valid }
+      expect(script).to_not be_valid
+    end
+
+    it "ensures company presence" do
+      script.company_id = nil
+      script.save
+
+      expect(script).to_not be_valid
+    end
+
+    it "ensures document is valid" do
+      script.document = "another"
+      script.save
+
+      expect(script).to_not be_valid
     end
 
     it "ensures script is valid" do
       script.save
 
-      it { should be_valid }
+      expect(script).to be_valid
     end
   end
 end
