f37b0996b09bbac394616e3c18bd70da7314d66a
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-11-19 15:03:32 -0300

    Defined two new methods that check if user has a valid document and checks if has valid document based of doc_type

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -9,6 +9,14 @@ class User < ApplicationRecord
          :jwt_authenticatable, :devise,
          jwt_revocation_strategy: JWTBlacklist
 
-  has_one_attached :user_document
   has_many :contracts
+  has_many :documents
+
+  def has_valid_document
+    return self.documents.where(valid: true)
+  end
+
+  def has_valid_document_where document_type
+    return self.documents.where(document_type: document_type)
+  end
 end
