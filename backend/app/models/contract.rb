10a68c56e22252da8176af5e82baf9b0a2acbe5e
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-11-26 00:38:22 -0300

    Missing a way to get contract absolute url to send to email

--- backend/app/models/contract.rb
+++ backend/app/models/contract.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 class Contract < ApplicationRecord
+  after_save :send_email_new_contract
+  
   belongs_to :company
   belongs_to :user
 
   has_one_attached :video
   has_one_attached :audio
   has_many_attached :image
+
+  def send_email_new_contract
+    SendgridMailer(self.user.email, { buttonURL: url_for()})
+  end
 end
