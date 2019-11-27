180cf2c2b38fd7dc6fe7c32e2a3cdb95188b5d03
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-11-26 21:49:24 -0300

    Removing default_url from production

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -82,5 +82,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: '3000' }
 end
